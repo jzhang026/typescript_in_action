e72074a58e9dfe5008769b20eaf6e379250d0bd8
zhangjiye <zhangjiye@mozat.com>
2019-08-06 18:00:46 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@
 // import "./part1.base/function"
 // import "./part1.base/class"
 import "./part1.base/class-interface"
+import "./part1.base/generic"
 let hello : string = 'hello type script'
 document.querySelectorAll('.app')[0].innerHTML = hello;
--- src/part1.base/generic.ts
+++ src/part1.base/generic.ts
@@ -1,19 +1,30 @@
-import { StringifyOptions } from "querystring";
+// function log<T>(value: T):T {
+//     console.log(value);
+//     return value;
+// }
 
-function log<T>(value: T):T {
-    console.log(value);
-    return value;
-}
-
-log<string[]>(['a','b']) 
-log(['a','b'])
+// log<string[]>(['a','b']) 
+// log(['a','b'])
 
 // type Log = <T>(value: T) => T
 // let myLog: Log = log
 
-interface Log <T = string>{
-    (value: T): T;
+// interface Log <T = string>{
+//     (value: T): T;
+// }
+
+// let myLog: Log = log;
+// myLog('1')
+
+class Log<T> {
+    run(value: T) {
+        console.log(value)
+        return value
+    }
 }
 
-let myLog: Log = log;
-myLog('1')
+let log1 = new Log<number>();
+log1.run(2)
+let log2 = new Log();
+log2.run(3);
+log2.run({a: 1, b: 2})
